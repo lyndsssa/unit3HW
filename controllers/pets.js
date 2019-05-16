a9328027834aa77934cc5704370e7e14830ef5e6
ecshaff <emmyshaff@gmail.com>
2019-05-15 20:33:38 -0700

    controllers created

--- controllers/pets.js
+++ controllers/pets.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-const Pet = require('../models/pets.js')
+const Pets = require('../models/pets.js')
 
 //INDEX route//
 router.get('/', (req, res) => {
   //res.send('index');
      Pets.find({}, (err, foundPets) => {
          res.json(foundPets)
+    });
+});
+
+//DELETE Route
+
+router.delete('/:id', (req, res) => {
+    Pets.findByIdAndRemove(req.params.id, (err, deletedPet) => {
+        res.json(deletedPet);
     });
 });
 
@@ -19,8 +27,12 @@ router.post('/', (req, res) => {
     });
 });
 
-
-
+// PUT Route
+router.put('/:id', (req, res) => {
+    Pets.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPet) => {
+        res.json(updatedPet);
+    });
+});
 
 
 
